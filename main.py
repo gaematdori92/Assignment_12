0c122102c7f505c4e863e4a127cbaa9d0588a6b8
gaematdori92 <seunghyunbest0902@gmail.com>
2026-05-13 18:31:41 +0900

    Fix file write mode bug

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def train_file_list_to_json(english_file_list: List[str], german_file_list: List
 
 def write_file_list(file_list: List[str], path: str) -> None:
     """Writes a list of strings to a file, each string on a new line"""
-    with open(path, 'r') as f:
+    with open(path, 'w') as f:
         for file in file_list:
             f.write('\n')
             
